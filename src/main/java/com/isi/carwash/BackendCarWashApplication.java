35ba35179eedd1e581bf1993df576aeef87a68b4
Roua-Ben-Emna <b.amnaroua@gmail.com>
2024-06-03 10:41:22 +0100

    first commit

---
+++ src/main/java/com/isi/carwash/BackendCarWashApplication.java
@@ -0,0 +1,13 @@
+package com.isi.carwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCarWashApplication.class, args);
+	}
+
+}
